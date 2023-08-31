e5e3935dff3f402729e19cf570ae390c7c41d636
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-31 19:17:34 +0000

    binary_tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,7 +8,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 	if (tree->left)
 		left = binary_tree_size(tree->left);
