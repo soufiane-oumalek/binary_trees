3899fc0cc6067172835545c3425fdb7b7e7e20cd
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-31 19:59:20 +0000

    binary_tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,15 +7,15 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t c = 0;
-	if (!tree)
+	if (tree == NULL)
 	{
 		return (0);
 	}
-	else
-	{
-		c += ((tree->left || tree->right) ? 1 : 0);
-		c += binary_tree_nodes(tree->left);
+	if (tree->left || tree->right)
+		c += 1;
+	if (tree->right)
 		c += binary_tree_nodes(tree->right);
-	}
-	return (c)
+	if (tree->left)
+		c += binary_tree_nodes(tree->left);
+	return (c);
 }
