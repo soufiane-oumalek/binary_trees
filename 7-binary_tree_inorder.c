a181ce22d103702528fe74abfeb8dea772619c4d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-31 19:03:02 +0000

    binary_tree

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,11 +6,12 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree & func)
+	if (tree == NULL || func == NULL)
 	{
-		func(tree->n);
-		binary_tree_inorder(tree->left, func);
-		func(tree->n);
-		binary_tree_inorder(tree->right, func);
+		return;
 	}
+	func(tree->n);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
