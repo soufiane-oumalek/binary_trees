6439714c1a9205adac77621906d696287db8b635
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-31 19:14:15 +0000

    binary_tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,8 +7,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lft = 0;
-	size_t rigt = 0;
+	size_t left = 0;
+	size_t right = 0;
 
 	if (!tree)
 	{
@@ -18,9 +18,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			lft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			rigt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((lft > rigt) ? 1 : rigt);
+		return ((left > right) ? left : right);
 	}
 }
