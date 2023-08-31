d7061b3fc3e2bfa1c5fab657811f4bb50ea1721f
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-31 18:58:04 +0000

    binary_tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,10 +6,12 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree & func)
+	if (tree == NULL || func == NULL)
 	{
-		func(tree->n)
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		return;
 	}
+	func(tree->n)
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+
 }
