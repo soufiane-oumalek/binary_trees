1d9ca72ad34aa6fb6b74cc3296d4fb36de2295ab
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-31 19:07:32 +0000

    binary_tree

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,9 +7,8 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree & func)
+	if (tree == NULL || func == NULL)
 	{
-		func(tree->n)
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
 		func(tree->n);
